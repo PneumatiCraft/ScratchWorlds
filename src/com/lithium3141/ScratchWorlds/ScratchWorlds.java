beef44a9303d456cc5ff28a6818dd02d2c88e0ed
Tim Ekl <lithium3141@gmail.com>
2011-06-05 14:39:17 -0500

    Integrate Permissions fully
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- src/com/lithium3141/ScratchWorlds/ScratchWorlds.java
+++ src/com/lithium3141/ScratchWorlds/ScratchWorlds.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
@@ -35,6 +36,7 @@ public class ScratchWorlds extends JavaPlugin {
 	
 	// Permissions interface
 	public PermissionHandler permissionHandler;
+	public static final String PERMISSION_ROOT_NAME = "ScratchWorlds";
 
 	@Override
 	public void onDisable() {
@@ -85,12 +87,19 @@ public class ScratchWorlds extends JavaPlugin {
 				return true;
 			}
 			
+			// Parse out the command
 			String subcommand = args[0];
 			String[] subargs = new String[args.length - 1];
 			for(int i = 0; i < subargs.length; i++) {
 				subargs[i] = args[i + 1];
 			}
 			
+			// Check permission
+			if(!this.checkPermission(sender, PERMISSION_ROOT_NAME + "." + subcommand)) {
+				sender.sendMessage(ChatColor.RED + "You do not have the necessary permission to run /" + COMMAND_NAME + " " + subcommand);
+			}
+			
+			// Dispatch the command
 			try {
 				// Reflection magic: find the class for the invoked command, instantiate it, and call its execute() method
 				Class<?> klass = Class.forName("com.lithium3141.ScratchWorlds.commands.SW" + this.capitalize(subcommand) + "Command");
@@ -119,6 +128,15 @@ public class ScratchWorlds extends JavaPlugin {
 	
 	private String capitalize(String arg) {
 		return arg.substring(0, 1).toUpperCase() + arg.substring(1).toLowerCase();
+	}
+	
+	boolean checkPermission(CommandSender sender, String permission) {
+		if(sender instanceof Player) {
+			return (this.permissionHandler != null && this.permissionHandler.has((Player)sender, permission)); 
+		} else {
+			// Running from console; always allow
+			return true;
+		}
 	}
 
 }
