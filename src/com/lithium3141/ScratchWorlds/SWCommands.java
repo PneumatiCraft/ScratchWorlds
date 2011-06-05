18c500eeabeb1632be25e54493011d1aa96bcb6c
Tim Ekl <lithium3141@gmail.com>
2011-06-05 00:56:56 -0500

    Implement in-plugin world marking/unmarking
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- src/com/lithium3141/ScratchWorlds/SWCommands.java
+++ src/com/lithium3141/ScratchWorlds/SWCommands.java
@@ -3,6 +3,7 @@ package com.lithium3141.ScratchWorlds;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 
@@ -13,8 +14,16 @@ public class SWCommands {
 		this.plugin = sw;
 	}
 	
+	private boolean checkArgLength(CommandSender sender, String[] args, int expected) {
+		if(args.length != expected) {
+			sender.sendMessage("Wrong number of arguments; expected " + expected + ", got " + args.length);
+			return false;
+		}
+		return true;
+	}
+	
 	public void list(CommandSender sender, String[] args) {
-		// TODO finish implementing
+		if(!this.checkArgLength(sender, args, 0)) return;
 		
 		// Figure out worlds
 		List<World> scratchWorlds = new ArrayList<World>();
@@ -36,5 +45,47 @@ public class SWCommands {
 		for(World w : otherWorlds) {
 			sender.sendMessage("    " + w.getName());
 		}
+	}
+	
+	private boolean validateWorld(CommandSender sender, String worldName) {
+		// Check that the world name is an actual world
+		if(this.plugin.getServer().getWorld(worldName) == null) {
+			sender.sendMessage(ChatColor.RED + "World " + worldName + " is not a loaded world");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public void mark(CommandSender sender, String[] args) {
+		if(!this.checkArgLength(sender, args, 1)) return;
+		
+		String worldName = args[0];
+		if(!this.validateWorld(sender, worldName)) return;
+		
+		// Check that this world is not already marked
+		if(this.plugin.scratchWorldNames.contains(worldName)) {
+			sender.sendMessage(ChatColor.RED + "World " + worldName + " is already a scratch world!");
+			return;
+		}
+		
+		this.plugin.scratchWorldNames.add(worldName);
+		sender.sendMessage("World " + worldName + " is now a scratch world.");
+	}
+	
+	public void unmark(CommandSender sender, String[] args) {
+		if(!this.checkArgLength(sender, args, 1)) return;
+		
+		String worldName = args[0];
+		if(!this.validateWorld(sender, worldName)) return;
+		
+		// Check that this world is currently marked
+		if(!this.plugin.scratchWorldNames.contains(worldName)) {
+			sender.sendMessage(ChatColor.RED + "World " + worldName + " is not currently a scratch world!");
+			return;
+		}
+		
+		this.plugin.scratchWorldNames.remove(worldName);
+		sender.sendMessage("World " + worldName + " is no longer a scratch world.");
 	}
 }
